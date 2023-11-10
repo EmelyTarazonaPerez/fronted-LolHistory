df8aa3c2e4a8b280a04275f0b3ebf9e838aff37b
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-11-10 17:32:08 -0500

    fix add/module-share

--- src/app/webside/webside.module.ts
+++ src/app/webside/webside.module.ts
@@ -3,18 +3,19 @@ import { CommonModule } from '@angular/common';
 import { WebsideRoutingModule } from './webside-routing.module';
 import { HomeComponent } from './page/home/home.component';
 import { ProfileComponent } from '../webside/component/profile/profile.component';
-import { ProgressComponent } from '../share/component/progress.component'
+import { ShareModule } from '../share/share.module';
+import { ProgressComponent } from '../share/component/progress.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     ProfileComponent,
-    ProgressComponent
 
   ],
   imports: [
     CommonModule,
     WebsideRoutingModule,
+    ShareModule
   ]
 })
 export class WebsideModule { }
