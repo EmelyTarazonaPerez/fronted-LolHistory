81158e074cc4e2a983c711829d33db813615f83a
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-11-10 17:25:04 -0500

    add share module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './webside/component/profile/profile.component';
-import { ProgressComponent } from './share/component/progress.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
-    ProgressComponent
   ],
   imports: [
     BrowserModule,
--- src/app/webside/webside.module.ts
+++ src/app/webside/webside.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebsideRoutingModule } from './webside-routing.module';
-
+import { HomeComponent } from './page/home/home.component';
+import { ProfileComponent } from '../webside/component/profile/profile.component';
+import { ProgressComponent } from '../share/component/progress.component'
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ProfileComponent,
+    ProgressComponent
+
+  ],
   imports: [
     CommonModule,
-    WebsideRoutingModule
+    WebsideRoutingModule,
   ]
 })
 export class WebsideModule { }
