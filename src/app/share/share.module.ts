3026e6e5f04457acdb17523c5c5379ea1b0f9b74
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-11-12 18:41:46 -0500

    add component navbar

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProgressComponent } from '../share/component/progress.component'
+import { ProgressComponent } from '../share/component/progress.component';
+import { NabvarComponent } from './component/nabvar/nabvar.component'
 
 
 @NgModule({
   declarations: [
-    ProgressComponent
+    ProgressComponent,
+    NabvarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ProgressComponent
+    ProgressComponent,
+    NabvarComponent
   ]
 })
 export class ShareModule { }
