dfdc76e1d73c4c1962627785c14ff17e22be781a
EmelyTarazonaPerez <emelydayannapt@ufps.edu.co>
2023-11-13 18:26:21 -0500

    add model DTO

---
+++ src/app/model/profile.ts
@@ -0,0 +1,10 @@
+import { noop } from "rxjs"
+
+interface Profile {
+  "id": String,
+  "puuid": String,
+  "name": String,
+  "profileIconId": number,
+  "summonerLevel": number,
+  "linkIcono": String
+}
